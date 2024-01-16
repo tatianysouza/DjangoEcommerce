59419ac15eef871b2626c6724688f5ad1864b4c5
tatiany <tatianysouza2018@gmail.com>
2024-01-16 16:24:43 -0300

    Implementando dados da compra e opções de pagamento

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -13,7 +13,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_total": 0, "get_cart_items": 0}
+        order = {"get_cart_total": 0, "get_cart_items": 0, "shipping":False}
         cartItems = order["get_cart_items"]
         
     products = Product.objects.all()
@@ -29,7 +29,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_total": 0, "get_cart_items": 0}
+        order = {"get_cart_total": 0, "get_cart_items": 0, "shipping":False}
         cartItems = order["get_cart_items"]
 
     context = {"items": items, "order": order, "cartItems":cartItems}
@@ -43,7 +43,7 @@ def checkout(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        order = {"get_cart_total": 0, "get_cart_items": 0}
+        order = {"get_cart_total": 0, "get_cart_items": 0, "shipping":False}
         items = []
         cartItems = order["get_cart_items"]
 
