5eb94511efcdc25ba08fb4e6189db20bf2182b7d
tatiany <tatianysouza2018@gmail.com>
2024-01-24 15:59:33 -0300

    Adicionando suporte para tamanhos e quantidades de produtos no admin

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import *
+class TamanhoProdutoInline(admin.TabularInline):
+    model = TamanhoProduto
+    extra = 1
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [TamanhoProdutoInline]
 
 admin.site.register(Cliente)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
 admin.site.register(Carrinho)
 admin.site.register(CarrinhoItem)
 admin.site.register(EnderecoEnvio)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -25,19 +25,34 @@ class Produto(models.Model):
         ('U', 'Unissex'),
     )
 
+    MARCAS = (
+        ('O', 'Olympikus'),
+        ('M', 'Mormaii'),
+        ('C', 'Converse'),
+        ('N', 'Nike'),
+        ('A', 'Adidas'),
+    )
+
+    DEPARTAMENTOS = (
+        ('E', 'Esporte'),
+        ('C', 'Calçados'),
+        ('A', 'Acessórios'),
+        ('R', 'Corrida'),
+    )
+
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     image = models.ImageField(null=True, blank=True)
     genero = models.CharField(max_length=1, choices=GENEROS, blank=True)
     cor = models.CharField(max_length=200, null=True, blank=True)
-    departamento_bs = models.CharField(max_length=200, null=True, blank=True)
+    departamento_bs = models.CharField(max_length=1, null=True, choices=DEPARTAMENTOS, blank=True)
     indicado_para = models.CharField(max_length=200, null=True, blank=True)
     material = models.CharField(max_length=200, null=True, blank=True)
     material_interno = models.CharField(max_length=200, null=True, blank=True)
     altura_do_cano = models.CharField(max_length=200, null=True, blank=True)
     solado = models.CharField(max_length=200, null=True, blank=True)
     peso_do_produto = models.FloatField(blank=True, null=True)
-    marca = models.CharField(max_length=200,null=True, blank=True)
+    marca = models.CharField(max_length=1, choices=MARCAS,null=True, blank=True)
     importante = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
@@ -54,6 +69,29 @@ class Produto(models.Model):
     class Meta:
         verbose_name = "Produto"
         verbose_name_plural = "Produtos"
+
+class TamanhoProduto(models.Model):
+    TAMANHOS = (
+        ('34', '34'),
+        ('35', '35'),
+        ('36', '36'),
+        ('37', '37'),
+        ('38', '38'),
+        ('39', '39'),
+        ('40', '40'),
+        ('41', '41'),
+        ('42', '42'),
+        ('43', '43'),
+        ('44', '44'),
+        ('45', '45'),
+    )
+
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    tamanho = models.CharField(max_length=2, choices=TAMANHOS)
+    quantidade = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.produto.name} - {self.tamanho}'
 
 
 class Carrinho(models.Model):
