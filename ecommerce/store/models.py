4ed66fbd1fabd5b6d942a83177d32490ba4dec6c
tatiany <tatianysouza2018@gmail.com>
2024-06-11 18:27:07 -0300

    Adicionando novos tamanhos e informações de produtos

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -66,7 +66,8 @@ class Produto(models.Model):
     solado = models.CharField(max_length=200, null=True, blank=True)
     peso_do_produto = models.FloatField(blank=True, null=True)
     marca = models.CharField(max_length=1, choices=MARCAS, null=True, blank=True)
-    importante = models.CharField(max_length=200, null=True, blank=True)
+    descricao = models.TextField(max_length=1400, null=True, blank=True)
+    importante = models.TextField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -91,6 +92,18 @@ def product_delete(sender, instance, **kwargs):
 
 class TamanhoProduto(models.Model):
     TAMANHOS = (
+        ("PP", "PP"),
+        ("P", "P"),
+        ("M", "M"),
+        ("G", "G"),
+        ("GG", "GG"),
+        ("XG", "XG"),
+        ("28", "28"),
+        ("29", "29"),
+        ("30", "30"),
+        ("31", "31"),
+        ("32", "32"),
+        ("33", "33"),
         ("34", "34"),
         ("35", "35"),
         ("36", "36"),
